2e22acdac3af9e8f11290a8c0a82f8075d1c4df4
Alex Martynov <sinner.in.me11@gmail.com>
2016-01-26 22:52:57 +0200

    Added some shit to class Note

--- src/lib/include/note.h
+++ src/lib/include/note.h
@@ -1,9 +1,35 @@
 #ifndef NOTE_H
 #define NOTE_H
+#include <iostream>
+#include <vector>
+
+using namespace std;
 
 class Note
 {
- public:
-  Note();
+private:
+	string title;
+	string path;
+	bool isCheckbox;
+	bool isRootNote;
+	int noteType;
+	Note* parentNote;
+	vector<Note*> notes;
+
+public:
+	enum types
+	{
+		root = 1,
+		text = 2,
+		pic = 3
+	};
+	Note(string _path, types _noteType);
+	bool setTitle(string _title);
+	string getTitle();
+	bool setPath(string _path);
+	string getPath();
+	int getType();
+	void addNote(string _title, string _path, types _noteKind);
+	static void loadContent(Note* &note);
 };
 #endif // NOTE_H
--- src/lib/src/note.cpp
+++ src/lib/src/note.cpp
@@ -1,5 +1,63 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
 #include "note.h"
 
-Note::Note()
-{
+using namespace std;
+
+Note::Note(string _path, types _noteType) {
+	path = _path;
+	noteType = _noteType;
+}
+
+bool Note::setTitle(string _title) {
+	if (_title != "") {
+		title = _title;
+		return true;
+	}
+	return false;
+}
+
+string Note::getTitle() {
+	return title;
+}
+
+bool Note::setPath(string _path) {
+	if (_path != "") {
+		path = _path;
+		return true;
+	}
+	return false;
+}
+
+string Note::getPath() {
+	return path;
+}
+
+void Note::addNote(string _title, string _path, types _noteKind) {
+	Note* newNote = new Note(_path, _noteKind);
+	newNote->setTitle(_title);
+	notes.push_back(newNote);
+}
+
+//Maybe will be relocated to read/write classes
+void Note::loadContent(Note* &note) {
+	if (note->noteType == types::text || types::root) {
+		ifstream in(note->path);
+		string s, line;
+		while (getline(in, line)) {
+			s += line + "\n";
+		}
+		cout << s;
+	}
+}
+
+int main() {
+	Note* note = new Note("c:\\1.txt", Note::types::root);
+	note->setTitle("NOTTTTEE");
+	note->addNote("KEK", "", Note::types::text);
+	cout << "Title: " << note->getTitle() << endl << endl << "Content:" << endl << endl;
+	Note::loadContent(note);
+	return 0;
 }
