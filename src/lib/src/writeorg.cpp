ec407f252235944c7b75dc8bf436f369fe335c50
Alex Martynov <sinner.in.me11@gmail.com>
2016-02-03 23:27:50 +0200

    Added methods write and writeNote for writing notes to file
    1) writeNote(...) - a method which writes into a file title of a root
    note;
    2) write(...) - recursive method which writes inner notes' titles and
    content into a file.

--- src/lib/src/writeorg.cpp
+++ src/lib/src/writeorg.cpp
@@ -1,24 +1,56 @@
 #include "../include/writeorg.h"
 
-void WriteOrg::writeNote(Note* &note) {}
-
-string WriteOrg::loadContent(Note* &note)
-{
-  string content = "";
-
-  if (note-> getType() == Note::text)
+void WriteOrg::writeNote(Note* note) {
+	if (note != NULL && note->getType() == Note::types::root)
 	{
-	  ifstream in(note->getPath().c_str());
-	  string line;
+		string filename = note->getPath();
+		ofstream out(filename, ios_base::trunc);
 
-	  while (getline(in, line))
+		string level = "*";
+		out << level << note->getTitle() << endl;
+		if (note->notes.size() != 0)
 		{
-		  content += line + "\n";
+			write(note, filename, level);
+		}
+		out.close();
+	}
+}
+
+void WriteOrg::write(Note* note, string filename, string level)
+{
+	ofstream out(filename, ios_base::app);
+	level += "*";
+	for (int i = 0; i < note->notes.size(); i++)
+	{
+		out << level << note->notes[i]->getTitle() << endl;
+		string content = WriteOrg::loadContent(note->notes[i]);
+		out << content << endl;
+		if (note->notes[i]->notes.size() != 0)
+		{
+			write(note->notes[i], filename, level);
 		}
 	}
-  else if (note->getType() == Note::pic)
-	{
-	  content = note->getPath();
-	}
-  return content;
+	out.close();
 }
+
+string WriteOrg::loadContent(Note* note)
+{
+	string content = "";
+
+	if (note->getType() == Note::text)
+	{
+		ifstream in(note->getPath().c_str());
+		string line;
+
+		while (getline(in, line))
+		{
+			content += line + "\n";
+		}
+	}
+	else if (note->getType() == Note::pic)
+	{
+		content = note->getPath();
+	}
+	return content;
+}
+
